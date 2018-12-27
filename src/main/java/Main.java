d7084df49e9577e315b4f377b755cfd495b7afaa
Marik <mixa.marik@gmail.com>
2018-12-27 23:42:26 +0200

    add buttons, fix update date

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,14 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends TelegramLongPollingBot {
     private static ArrayList<String> employees = new ArrayList<String>();
@@ -26,21 +30,50 @@ public class Main extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
         if (update.hasMessage() && update.getMessage().hasText()) {
-            long chat_id = update.getMessage().getChatId();
-            String result = "";
-            for (int i = 0; i < employees.size(); i++) {
-                result += employees.get(i) + " ---> " + new MyRequest().getWorkLoad(employees.get(i)) + "\n";
+            StringBuilder result = new StringBuilder();
+            for (String employee : employees) {
+                result.append(employee).append(" ---> ").append(new MyRequest().getWorkLoad(employee)).append("\n");
             }
 
-            SendMessage message = new SendMessage()
-                    .setChatId(chat_id)
-                    .setText(result);
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            String s = message.getText();
+            if ("/start".equals(s)) {
+                sendMsg(message, "Привет, я считаю загрузку всего региона");
+
+            } else if ("За сегодня".equals(s)) {
+                sendMsg(message, result.toString());
+
+            } else if ("За месяц".equals(s)) {
+                sendMsg(message, "Так я еще не умею ;)");
+
+            } else {
+                sendMsg(message, ".i.");
             }
+        }
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add("За сегодня");
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        keyboardSecondRow.add("За месяц");
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
--- src/main/java/MyRequest.java
+++ src/main/java/MyRequest.java
@@ -4,33 +4,69 @@ import org.jsoup.helper.HttpConnection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Authenticator;
+import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MyRequest {
-    private static String viewstate = new TextHelper().getText("viewstate.txt");
-    private static String eventValidation = new TextHelper().getText("eventValidation.txt");
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-    private static String date = dateFormat.format(new Date());
+class MyRequest {
     private static String userName = new TextHelper().getText("login.txt");
     private static String password = new TextHelper().getText("pwd.txt");
+    private String viewState;
+    private String eventValidation;
+    private String urlStr = "http://service.triolan.com/Abonent/QuestionsAbon.aspx";
 
-    public String getWorkLoad(String employee) {
+    MyRequest() {
+        try {
+            Document document = Jsoup.parse(getAuthenticatedResponse(urlStr, userName, password));
+            Elements elements = document.getElementsByTag("input");
+            viewState = elements.get(1).val();
+            eventValidation = elements.get(4).val();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getAuthenticatedResponse(final String urlStr,
+                                            final String userName,
+                                            final String password) throws IOException {
+        StringBuilder response = new StringBuilder();
         Authenticator.setDefault(new Authenticator() {
             @Override
             public PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(userName, password.toCharArray());
             }
         });
-        Connection connection = HttpConnection.connect("http://service.triolan.com/Abonent/QuestionsAbon.aspx")
+        URL urlRequest = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) urlRequest.openConnection();
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        conn.setRequestMethod("GET");
+        InputStream stream = conn.getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+        String str;
+        while ((str = in.readLine()) != null) {
+            response.append(str).append("\n");
+        }
+        in.close();
+        return response.toString();
+    }
+
+    String getWorkLoad(String employee) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String date = dateFormat.format(new Date());
+        Connection connection = HttpConnection.connect(urlStr)
                 .timeout(10000)
                 .ignoreHttpErrors(true)
                 .data("ctl00_ToolkitScriptManager1_HiddenField", ";;AjaxControlToolkit, Version=4.1.51116.0, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:ru-RU:fd384f95-1b49-47cf-9b47-2fa2a921a36a:f2c8e708:de1feab2:720a52bf:f9cec9bc:589eaa30:698129cf:7a92f56c:fcf0e993:fb9b4c57:ccb96cf9:a67c2700:8613aea7:3202a5a2:ab09e3fe:87104b7c:be6fb298")
                 .data("__EVENTTARGET", "ctl00$cph_main$tbEmpl")
-                .data("__VIEWSTATE", viewstate)
+                .data("__VIEWSTATE", viewState)
                 .data("__VIEWSTATEGENERATOR", "5AC7BC9A")
                 .data("__VIEWSTATEENCRYPTED", "")
                 .data("__EVENTVALIDATION", eventValidation)
@@ -54,15 +90,26 @@ public class MyRequest {
             e.printStackTrace();
             System.out.println("тут ошибка");
         }
-        Document doc2 = Jsoup.parse(response.body());
-        Element table = doc2.getElementById("ctl00_cph_main_gvQuestions");
-        Elements rows = table.select("tr");
+        Document doc2 = null;
+        if (response != null) {
+            doc2 = Jsoup.parse(response.body());
+        }
+        Element table = null;
+        if (doc2 != null) {
+            table = doc2.getElementById("ctl00_cph_main_gvQuestions");
+        }
+        Elements rows = null;
+        if (table != null) {
+            rows = table.select("tr");
+        }
         int zagruz = 0;
-        for (int i = 1; i < rows.size(); i++) {
-            Element row = rows.get(i);
-            Elements cols = row.select("td");
-            if (cols.get(19).text().length() > 0){
-                zagruz += Integer.parseInt(cols.get(19).text());
+        if (rows != null) {
+            for (int i = 1; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                if (cols.get(19).text().length() > 0) {
+                    zagruz += Integer.parseInt(cols.get(19).text());
+                }
             }
         }
         return zagruz / 60 + "ч. : " + zagruz % 60 + " мин.";
